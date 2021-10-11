095b3c1a948e34e16370206c6e9ff5993fd5d463
Meninggal <88880334+ZenTod@users.noreply.github.com>
2021-10-11 23:24:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async(req, res) => {
-	res.sendFile(__path + '/views/index.html')
+	res.sendFile(__path + 'index.html')
 })
 router.get('/css/style.css', async(req, res) => {
-	res.sendFile(__path + '/views/css/style.css')
+	res.sendFile(__path + 'assets/css/styles.css')
 })
 
 //Kalo page yang lu cari engga ada, nanti muncul ini
