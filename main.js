0576e5eeb64c1afa4b35bcf4ab1b93a2f6b94253
AntiWibu <88880334+ZenTod@users.noreply.github.com>
2021-10-13 00:43:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async(req, res) => {
-	res.sendFile(__path + '/index.html')
+	res.sendFile(__path + 'index.html')
 })
 router.get('/css/style.css', async(req, res) => {
-	res.sendFile(__path + '/assets/css/styles.css')
+	res.sendFile(__path + 'assets/css/styles.css')
 })
 
 //Kalo page yang lu cari engga ada, nanti muncul ini
 router.use(function (req, res) {
 res.status(404)
-  res.sendFile(__path + '/views/404.html')
+  res.sendFile(__path + 'views/404.html')
 });
 
 
