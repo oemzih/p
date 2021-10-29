52576d01965da352f226da9a7f84c8fa1d64d3b6
Zen <88880334+Zenn666@users.noreply.github.com>
2021-10-29 22:15:17 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,8 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "ZenTod", { responsive: true });
+    new GitHubCalendar("#github-graph", "Zenn666", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "ZenTod", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "Zenn666", selector: "#ghfeed" });
