08cf5db6764cc93b8747c26232c8d97ed18eaf76
Zen <88880334+Zenn666@users.noreply.github.com>
2021-12-05 17:47:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/docs', async(req, res) => {
+router.get('../index.html', async(req, res) => {
 	res.sendFile(__path + '../index.html')
 })
 
