08e590f1f457784ff67fc3872ea0c481f0d68c3a
Zen <88880334+Zenn666@users.noreply.github.com>
2021-12-05 18:08:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/docs', async(req, res) => {
-	res.sendFile(__path + '../index.html')
+	res.sendFile(__path + '/index.html')
 })
 
 router.get('/assets/css/styles.css', async(req, res) => {
-	res.sendFile(__path + '../assets/css/styles.css')
+	res.sendFile(__path + '/assets/css/styles.css')
 })
 
 router.get('/assets/js/main.js', async(req, res) => {
-	res.sendFile(__path + '../assets/js/main.js')
+	res.sendFile(__path + '/assets/js/main.js')
 }
 
 
 //Kalo page yang lu cari engga ada, nanti muncul ini
 router.use(function (req, res) {
 res.status(404)
-  res.sendFile(__path + '../views/404.html')
+  res.sendFile(__path + '/views/404.html')
 });
 
 
