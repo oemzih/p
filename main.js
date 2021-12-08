7420432fc5ec93a3294737d2c30065f139096ab3
Zen <88880334+Zenn666@users.noreply.github.com>
2021-12-08 07:44:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ router.get('/', async(req, res) => {
 	res.sendFile(__path + '/views/load.html')
 })
 
-router.get('/views/load.css', async(req, res) => {
-	res.sendFile(__path + '/views/load.css')
-})
-
 router.get('/docs', async(req, res) => {
 	res.sendFile(__path + '/index.html')
+})
+
+router.get('/views/load.css', async(req, res) => {
+	res.sendFile(__path + '/views/load.css')
 })
 
 router.get('/assets/css/styles.css', async(req, res) => {
