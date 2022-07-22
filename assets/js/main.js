a1627c360677db9fc9b7172f345cfc9d0b59fe0f
Zen <88880334+zexyzx@users.noreply.github.com>
2022-07-22 17:30:14 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,8 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "Znzn11", { responsive: true });
+    new GitHubCalendar("#github-graph", "zexyzx", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "Znzn11", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "zexyzx", selector: "#ghfeed" });
